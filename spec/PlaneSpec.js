ab334223d89b4f565ceccbf8b7cfc51014128f49
krisn64209 <krisnorris@gmail.com>
2019-11-15 11:13:31 +0000

    added unit test to Plane to check land function

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -2,12 +2,19 @@
 
 describe('Plane', function(){
   var plane;
+  var airport;
 
   beforeEach(function(){
     plane = new Plane();
+    airport = jasmine.createSpyObj('airport',['landing']);
   });
 
   it('can create new instances of Plane', function(){
     expect(plane).toBeInstanceOf(Plane);
+  });
+
+  it('can land at an Airport', function(){
+    plane.land(airport);
+    expect(airport.landing).toHaveBeenCalledWith(plane);
   });
 });
